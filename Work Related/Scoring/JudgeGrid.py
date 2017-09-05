0ac6f9d88a107a4abbe83d01ab976d6abb9f35f5
taylor-hutyra <taylor.hutyra@go.tarleton.edu>
2017-09-05 16:49:59 -0500

    final save state, note the button is not working yet

--- Work Related/Scoring/JudgeGrid.py
+++ Work Related/Scoring/JudgeGrid.py
@@ -1,8 +1,21 @@
 from tkinter import *
+import random
+
+
 
 ROWS = 10
 COLUMNS = 10
+COLOR = ["grey"]*ROWS*COLUMNS
+for i,x in enumerate(COLOR):
+    if 0.75 < random.random():
+        COLOR[i] = "purple"
 
+
+def button_click(i,j):
+    if self.cget('bg') is "grey":
+        self.button.configure(bg="purple")
+    else:
+        self.button.configure(bg="grey")
 #Create & Configure root
 root = Tk()
 Grid.rowconfigure(root, 0, weight=1)
@@ -17,7 +30,7 @@ for row_index in range(ROWS):
     Grid.rowconfigure(frame, row_index, weight=1)
     for col_index in range(COLUMNS):
         Grid.columnconfigure(frame, col_index, weight=1)
-        btn = Button(frame) #create a button inside frame
+        btn = Button(frame, bg = COLOR[row_index*COLUMNS+col_index], command = button_click) #create a button inside frame
         btn.grid(row=row_index, column=col_index, sticky=N+S+E+W)
 
 root.mainloop()
