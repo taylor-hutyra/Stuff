13caa519507d570491427dd22341fa53b92ce7c5
taylor-hutyra <taylor.hutyra@go.tarleton.edu>
2017-09-06 09:54:10 -0500

    Buttons are working!

--- Work Related/Scoring/JudgeGrid.py
+++ Work Related/Scoring/JudgeGrid.py
@@ -1,37 +1,37 @@
 from tkinter import *
-import random
 
-
-
+color="purple"
+default_color="white"
 ROWS = 10
 COLUMNS = 10
-COLOR = ["grey"]*ROWS*COLUMNS
-for i,x in enumerate(COLOR):
-    if 0.75 < random.random():
-        COLOR[i] = "purple"
+button_list = [default_color]*(ROWS*COLUMNS)
 
+def main():
+    window = Tk()
+    Grid.rowconfigure(window, 0, weight=1)
+    Grid.columnconfigure(window, 0, weight=1)
+    frame=Frame(window)
+    frame.grid(row=0, column=0, sticky=N+S+E+W)
+    for x in range(ROWS):
+        Grid.rowconfigure(frame, x, weight=1)
+        for y in range(COLUMNS):
+            Grid.columnconfigure(frame, y, weight=1)
+            b = Button(frame, bg=default_color, activebackground=default_color)
+            b.grid(column=y, row=x)
+            # creating the callback with "b" as the default parameter bellow "freezes" its value pointing
+            # to the button created in each run of the loop.
+            b["command"] = lambda b=b, x=x, y=y: click(b, x, y)
+            b.grid(row=x, column=y, sticky=N+S+E+W)
+    return window
 
-
-def button_click(i,j):
-    if self.cget('bg') is "grey":
-        self.button.configure(bg="purple")
+def click(button,x,y):
+    if button_list[x*ROWS+y] is color:
+        button_list[x*ROWS+y] = default_color
+        button["bg"] = default_color
+        button["activebackground"] = default_color
     else:
-        self.button.configure(bg="grey")
-#Create & Configure root
-root = Tk()
-Grid.rowconfigure(root, 0, weight=1)
-Grid.columnconfigure(root, 0, weight=1)
-
-#Create & Configure frame
-frame=Frame(root)
-frame.grid(row=0, column=0, sticky=N+S+E+W)
-
-#Create a 5x10 (rows x columns) grid of buttons inside the frame
-for row_index in range(ROWS):
-    Grid.rowconfigure(frame, row_index, weight=1)
-    for col_index in range(COLUMNS):
-        Grid.columnconfigure(frame, col_index, weight=1)
-        btn = Button(frame, bg = COLOR[row_index*COLUMNS+col_index], command = button_click) #create a button inside frame
-        btn.grid(row=row_index, column=col_index, sticky=N+S+E+W)
-
-root.mainloop()
+        button_list[x*ROWS+y] = color
+        button["bg"] = color
+        button["activebackground"] = color
+w = main()
+mainloop()
