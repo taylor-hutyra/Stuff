198f4fab97c74ee6e1b641e5ad93b0934c761053
taylor-hutyra <taylor.hutyra@go.tarleton.edu>
2017-09-12 16:46:33 -0500

    Added outside function to modify a global variable, the buttons react accordingly, this will be good for getting data from guidebook

--- Work Related/Scoring/JudgeGrid.py
+++ Work Related/Scoring/JudgeGrid.py
@@ -5,7 +5,10 @@ color="purple"
 default_color="white"
 ROWS = 10
 COLUMNS = 10
-button_list = [default_color]*(ROWS*COLUMNS)
+NUMJUDGES = 2
+button_list = []
+for i in range(ROWS*COLUMNS):
+    button_list.append([default_color, 2])
 
 def main():
     window = Tk()
@@ -17,26 +20,40 @@ def main():
         Grid.rowconfigure(frame, x, weight=1)
         for y in range(COLUMNS):
             Grid.columnconfigure(frame, y, weight=1)
-            b = Button(frame,text = str(x)+str(y), bg=default_color, activebackground=default_color)
+            b = Button(frame,text = str(x)+str(y) + '- NEEDS ' + str(NUMJUDGES), bg=default_color, activebackground=default_color)
             b.grid(column=y, row=x)
             # creating the callback with "b" as the default parameter bellow "freezes" its value pointing
             # to the button created in each run of the loop.
             b["command"] = lambda b=b, x=x, y=y: click(b, x, y)
             b.grid(row=x, column=y, sticky=N+S+E+W)
+    Grid.columnconfigure(frame, COLUMNS, weight=1)
+    Grid.rowconfigure(frame, int(ROWS/2), weight=1)
+    b = Button(frame, text = 'update', bg = default_color, activebackground = default_color)
+    b["command"] = lambda : update()
+    b.grid(column = COLUMNS, row = int(ROWS/2), sticky = N+S+E+W)
     return window
 
 def click(button,x,y):
-    if button_list[x*ROWS+y] is color:
-        button_list[x*ROWS+y] = default_color
+    if button_list[x*ROWS+y][0] is color:
+        button_list[x*ROWS+y][0] = default_color
+        button_list[x*ROWS+y][1] -= 1
         button["bg"] = default_color
         button["activebackground"] = default_color
-    elif button_list[x*ROWS+y] is default_color:
-        button_list[x*ROWS+y] = color
+        button["text"] = str(x)+str(y)+ '- NEEDS ' + str(button_list[x*ROWS+y][1])
+    elif button_list[x*ROWS+y][0] is default_color:
+        button_list[x*ROWS+y][0] = color
         button["bg"] = color
         button["activebackground"] = color
-    if random() < 0.14:
-        button_list[x*ROWS+y] = "grey"
+        button["text"] = str(x)+str(y) + '- JUDGING'
+    if button_list[x*ROWS+y][1] is 0:
+        button_list[x*ROWS+y][0] = "grey"
         button["bg"] = "grey"
         button["activebackground"] = "grey"
+        button["text"] = str(x)+str(y)+ '- DONE'
+    print(button_list)
+
+def update():
+    global color
+    color = 'red'
 w = main()
 mainloop()
